aa70f9d22adabcd959bd7bf9e7f399e684cbc926
Spacs21 <behaksr123@gmail.com>
2024-10-25 07:38:36 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.scss'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import Category from './components/category/Category'
+import Products from './components/products/Products'
+import About from './components/about/About'
+import Subscribe from './components/subscribe/Subscribe'
+import Footer from './components/footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="color">
+      <Header/>
+      <Hero/>
+    </div>
+    <Category/>
+    <Products/>
+    <About/>
+    <Subscribe/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Footer.scss"
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="container">
+            <div className="footer__text">
+                <p>Copyright @ 2022 | Mohid</p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
